e808a0baf067e1b684ac698299b49bc8c9bb1480
ZeZunStyler <162829852+DedeHead@users.noreply.github.com>
2024-03-09 19:34:09 +0100

    Update repo.js

--- repo.js
+++ repo.js
@@ -3,7 +3,7 @@
     "title": "ZeZunStyler's Mods",
     "contact": ".theoofer",
     "servers": [
-        "https://mirrors.thpatch.net/Kogasas_Mods/"
+        "https://mirrors.thpatch.net/ZeZunStyler/Touhou6FlowerEdition/"
     ],
     "patches": {
         "Touhou6FlowerEdition": "Adds Rin Satsuki to Eosd(WIP)"
