c8474d2ebfacb2890166e1445024894e7f339ec3
ZeZunStyler <162829852+DedeHead@users.noreply.github.com>
2024-03-09 19:19:31 +0100

    Add files via upload

---
+++ Touhou6FlowerEdition/patch.js
@@ -0,0 +1,12 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order"
+	],
+	"id": "Touhou6FlowerEdition",
+	"servers": [
+		"https://mirrors.thpatch.net/Kogasas_Mods/RinSatsukiMod/"
+	],
+	"title": "Touhou6FlowerEdition (06) Adds Rin Satsuki to Eosd(WIP)"
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "id": "DedeHead",
+    "title": "ZeZunStyler's Mods",
+    "contact": ".theoofer",
+    "servers": [
+        "https://mirrors.thpatch.net/Kogasas_Mods/"
+    ],
+    "patches": {
+        "Touhou6FlowerEdition": "Adds Rin Satsuki to Eosd(WIP)"
+    }
+}
